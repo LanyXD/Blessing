85fdcdc45843b862dc6c6baf9c6e69426cb94821
CrisKroos25 <141697280+CrisKroos25@users.noreply.github.com>
2026-04-30 11:20:12 -0600

    Se corrigio bug que no guardaba descripcion de cada producto

--- apps/inventory/serializers.py
+++ apps/inventory/serializers.py
@@ -4,9 +4,43 @@ from decimal import Decimal
 
 
 class ItemSerializer(serializers.ModelSerializer):
+
+    image = serializers.ImageField(use_url=True, required=False, allow_null=True)
+    description = serializers.SerializerMethodField()
+
     class Meta:
         model  = Item
         fields = '__all__'
+    
+    def get_description(self, obj):
+        """Obtiene description desde la tabla específica según el tipo."""
+        if obj.type == 'bundle' and hasattr(obj, 'bundle'):
+            return obj.bundle.description
+        if obj.type == 'product' and hasattr(obj, 'product'):
+            return obj.product.description
+        if obj.type == 'supply' and hasattr(obj, 'supply'):
+            return obj.supply.description
+        return ''
+
+    def update(self, instance, validated_data):
+        description = self.context['request'].data.get('description', None)
+
+        # Actualiza los campos de la tabla Item normalmente
+        instance = super().update(instance, validated_data)
+
+        # Guarda description en la tabla correcta según el tipo
+        if description is not None:
+            if instance.type == 'bundle' and hasattr(instance, 'bundle'):
+                instance.bundle.description = description
+                instance.bundle.save(update_fields=['description'])
+            elif instance.type == 'product' and hasattr(instance, 'product'):
+                instance.product.description = description
+                instance.product.save(update_fields=['description'])
+            elif instance.type == 'supply' and hasattr(instance, 'supply'):
+                instance.supply.description = description
+                instance.supply.save(update_fields=['description'])
+
+        return instance
 
     # validate_<field> asocia el error al campo correcto
     # DRF lo ejecuta antes que validate() general
