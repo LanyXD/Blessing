c4469fcee5db707dbeda2296f8e0b82543a009ae
LanyXD <andyml2639@gmail.com>
2026-05-01 16:30:43 -0600

    refactor: update sales/urls.py for better structure

--- apps/sales/urls.py
+++ apps/sales/urls.py
@@ -1,10 +1,18 @@
-# sales/urls.py
-
 from django.urls import path
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework.reverse import reverse
 from .views import SaleListCreateView, SaleProductListView
 
-urlpatterns = [
-    path('sales/', SaleListCreateView.as_view(), name='sale-list-create'),
-    path('sales/products/', SaleProductListView.as_view(), name='sale-product-list'),
+@api_view(['GET'])
+def sales_root(request, format=None):
+    return Response({
+        'sales':    reverse('sale-list-create', request=request, format=format),
+        'products': reverse('sale-product-list', request=request, format=format),
+    })
 
+urlpatterns = [
+    path('',          sales_root,                        name='sales-root'),
+    path('list/',     SaleListCreateView.as_view(),      name='sale-list-create'),
+    path('products/', SaleProductListView.as_view(),     name='sale-product-list'),
 ]
